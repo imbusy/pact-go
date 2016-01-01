b9cee522d6eb00516a70c744756bb07dd7e022c0
Gurnam Madan <gs.madan@gmail.com>
2016-01-01 14:22:06 +1100

    Added tests for response matcher.

---
+++ comparers/http_response_matcher_test.go
@@ -0,0 +1,55 @@
+package comparers
+
+import (
+	"bytes"
+	"github.com/SEEK-Jobs/pact-go/provider"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+type matchResponseTest struct {
+	exp       *provider.Response
+	act       *http.Response
+	diffCount int
+	diffPath  string
+}
+
+var (
+	matchResponseTestData = []*matchResponseTest{
+		&matchResponseTest{buildTestProviderResponse(200, nil, ""), &http.Response{StatusCode: 201}, 1, "[\"status\"]"},
+		&matchResponseTest{buildTestProviderResponse(200, http.Header{"Content-Type": {"application/json"}}, ""), &http.Response{StatusCode: 200}, 1, "[\"header\"]"},
+		&matchResponseTest{buildTestProviderResponse(200, nil, `{"name":"John Doe"}`), &http.Response{StatusCode: 200}, 1, "[\"body\"]"},
+		&matchResponseTest{buildTestProviderResponse(200, http.Header{"Content-Type": {"application/json"}}, `{"name":"John Doe"}`), buildTestHttpResponse(200, http.Header{"Content-Type": {"application/json"}}, `{"name":"John Doe"}`), 0, ""},
+	}
+)
+
+func buildTestProviderResponse(status int, h http.Header, body string) *provider.Response {
+	pr := provider.NewJSONResponse(200, h)
+	pr.SetBody(body)
+	return pr
+}
+
+func buildTestHttpResponse(status int, h http.Header, body string) *http.Response {
+	return &http.Response{
+		StatusCode:    status,
+		Header:        h,
+		Body:          ioutil.NopCloser(bytes.NewBufferString(body)),
+		ContentLength: int64(len(body)),
+	}
+}
+
+func Test_MatchResponse_Scenarios(t *testing.T) {
+	for _, test := range matchResponseTestData {
+		diff, err := MatchResponse(test.exp, test.act)
+		if err != nil {
+			t.Error(err)
+		}
+		if len(diff) != test.diffCount {
+			t.Errorf("expected diff count to be %d, but actual count id %d", test.diffCount, len(diff))
+		} else if len(diff) > 0 && !strings.Contains(diff.Error(), test.diffPath) {
+			t.Errorf("expected diff at %s, but got %s", test.diffPath, diff.Error())
+		}
+	}
+}

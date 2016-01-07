8ac744ea2b63739f8dbec21765c49278958f17d0
James Brady <james@jmsbrdy.com>
2016-01-07 10:15:34 +0000

    Add tests for missing provider or consumer names in pacts.

--- io/pact_file_test.go
+++ io/pact_file_test.go
@@ -4,14 +4,40 @@ import "testing"
 
 func Test_Validate_ValidFile(t *testing.T) {
 	path := "../pact_examples/consumer-provider.json"
-	r := NewPactFileReader(path)
+	p := readPactFile(t, path)
 
+	if err := p.Validate(); err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_Validate_MissingProvider(t *testing.T) {
+	path := "./pactNoProviderSpec.json"
+	p := readPactFile(t, path)
+
+	expectError(t, p.Validate(), errEmptyProvider)
+}
+
+func Test_Validate_MissingConsumer(t *testing.T) {
+	path := "./pactNoConsumerSpec.json"
+	p := readPactFile(t, path)
+
+	expectError(t, p.Validate(), errEmptyConsumer)
+}
+
+func readPactFile(t *testing.T, path string) *PactFile {
+	r := NewPactFileReader(path)
 	p, err := r.Read()
 	if err != nil {
 		t.Error(err)
 	}
+	return p
+}
 
-	if err := p.Validate(); err != nil {
-		t.Error(err)
+func expectError(t *testing.T, actual, expected error) {
+	if actual == nil {
+		t.Error("expected an error")
+	} else if actual != expected {
+		t.Error("got %v error, expected %v", actual, expected)
 	}
 }
